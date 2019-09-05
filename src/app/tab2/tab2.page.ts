518bbbf805f29afdb1fd17cb6d99db2cd27c01a7
chenyufeng <411610370@qq.com>
2019-09-05 13:09:20 +0800

    1

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+declare var BMap: any;
+declare var BMAP_ANIMATION_BOUNCE: any;
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  list: [];
+  @ViewChild('mapElement', null) mapElement: ElementRef;
+  constructor(private http: HttpClient) {
+    this.http.get('http://www.tonggeedu.com/test2/data.json').subscribe((d: []) => {
+      console.log(d);
+      this.list = d;
+    })
+
+setTimeout(e=>{
+  var map = new BMap.Map(this.mapElement.nativeElement);
+  var point = new BMap.Point(116.404, 39.915);
+  map.centerAndZoom(point, 15);
+  var marker = new BMap.Marker(point);  // 创建标注
+  map.addOverlay(marker);               // 将标注添加到地图中
+  marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+},1000)
+    
+  }
+
+
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private http:HttpClient) {
+
+    this.http.get('http://www.tonggeedu.com/test2/data.json').subscribe(d=>{
+      console.log(d);
+    })
+
+
+  }
+
 
 }
